ae62b9013782da9759e3dd3c760fdc5678ddb85e
Greg Baker <ggbaker@sfu.ca>
2015-02-20 12:49:18 -0800

    work on gigi's model

--- gigi.py
+++ gigi.py
@@ -6,7 +6,8 @@ helper_states = 5
 
 def phi(e, p, *helpers):
     if p > 0:
-        return 0
+        # can't be pregnant at menopause
+        return -1
     else:
         return sum(1 for h in helpers if h>0)
 
@@ -15,11 +16,15 @@ def foraging_results(e, p, helpers):
     # how many people are helping here?
     workers = 1 + sum(1 for h in helpers if h==(helper_states-1))
     dependants = 1 + sum(1 for h in helpers if h>0)
-    surplus = workers*2 - dependants
+    surplus = workers*2.5 - dependants
     # what's the gain for each person because of it?
     food_gain = surplus * 0.1
 
-    return e+food_gain, tuple(h+food_gain for h in helpers)
+    return (
+        e+food_gain,
+        tuple((h+food_gain if h else 0) for h in helpers),
+        surplus
+    )
 
 def possibilities(model, t, e, p, *helpers):
     helpers = tuple(helpers)
@@ -29,44 +34,68 @@ def possibilities(model, t, e, p, *helpers):
         possib.otherwise(0, qual=0.0, descr="starved")
         return possib
 
-    e, helpers = foraging_results(e, p, helpers)
+    e, helpers, surplus = foraging_results(e, p, helpers)
+    note = ' (%s)' % (surplus)
 
     if p == 9:
+        # give birth: a 0 helper becomes a 1 helper
+        helpers = list(helpers)
+        for i, h in enumerate(helpers):
+            if h == 0:
+                helpers[i] = 1
+                break
+        else:
+            # already full of kids
+            possib.otherwise(0, qual=-1, descr="excess kids!")
+            return possib
+        helpers = tuple(helpers)
+
         possib.add(
             decision=0,
             qualgain=0.0,
             prob=1.0,
-            nextstate=(t+1, e, 0) + helpers,
-            descr="give birth" )
+            nextstate=(t+1, e-1, 0) + helpers,
+            descr="give birth" + note )
 
     elif p > 0:
-        possib.add(
-            decision=0,
-            qualgain=0.0,
-            prob=1.0,
-            nextstate=(t+1, e-0.1, p+1) + helpers,
-            descr="stay pregnant" )
+        # pregnant
+        if e == 1:
+            # starving... pregnancy terminates
+            possib.add(
+                decision=0,
+                qualgain=0.0,
+                prob=1.0,
+                nextstate=(t+1, e-1, 0) + helpers,
+                descr="pregnancy terminated" + note )
+        else:
+            # be pregnant
+            possib.add(
+                decision=0,
+                qualgain=0.0,
+                prob=1.0,
+                nextstate=(t+1, e-0.1, p+1) + helpers,
+                descr="stay pregnant" + note )
 
     else:
         possib.add(
             decision=0,
             qualgain=0,
             prob=1.0,
-            nextstate=(t+1, e+0.1, 0) + helpers,
-            descr="abstain" )
+            nextstate=(t+1, e, 0) + helpers,
+            descr="abstain" + note )
 
         possib.add(
             decision=1,
             qualgain=0,
-            prob=0.7,
-            nextstate=(t+1, e, 1) + helpers,
-            descr="get pregnant" )
+            prob=0.2,
+            nextstate=(t+1, e-1, 1) + helpers,
+            descr="get pregnant" + note )
         possib.add(
             decision=1,
             qualgain=0,
-            prob=0.3,
-            nextstate=(t+1, e+0.1, 0) + helpers,
-            descr="didn't get pregnant" )
+            prob=0.8,
+            nextstate=(t+1, e, 0) + helpers,
+            descr="didn't get pregnant" + note )
 
     return possib
 
@@ -85,14 +114,22 @@ def buildmodel():
             possible_outcomes=possibilities,
             )
 
-def main():
+def generate_arrays():
     model = buildmodel()
-
     model.fill_terminal_quality()
     model.fill_quality(max)
-    
-    for e in range(10):
-        print model.quality(0, e, 0, 0, 0)
+
+def print_step(model, dec, descr, t, *state):
+    print dec, descr, t, state
+
+def monte():
+    model = buildmodel()
+    model.read_files()
+
+    model.monte_carlo_run(
+        start=(0, 4, 0, 0, 0),
+        report_step=print_step,
+    )
 
 def profile():
     import cProfile, pstats
@@ -102,7 +139,8 @@ def profile():
 
 
 if __name__ == '__main__':
-    main()
+    generate_arrays()
+    monte()
     #profile()
 
 
